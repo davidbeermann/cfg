89e78a06dc286977984bbfc0d60a6d248612c494
David Beermann <mail@davidbeermann.com>
2019-10-14 12:42:27 +0200

    extend post install script

--- scripts/post_install.sh
+++ scripts/post_install.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-# Usefull binaries from distribution 
+# Usefull binaries from distribution
 sudo apt update --quiet && sudo apt install --yes \
   gnome-tweaks \
-  google-chrome-stable
+  google-chrome-stable \
+  htop \
+  peek \
+  slack-desktop \
+  thunderbird
+
+# Increase the amount of inotify file watchers
+# https://github.com/guard/listen/wiki/Increasing-the-amount-of-inotify-watchers
+echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
