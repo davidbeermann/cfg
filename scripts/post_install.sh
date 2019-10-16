785b1f25a3a73d52e9470582fe49155df131ac13
David Beermann <mail@davidbeermann.com>
2019-10-16 13:15:52 +0200

    add resources on file watchers

--- scripts/post_install.sh
+++ scripts/post_install.sh
@@ -13,4 +13,6 @@ sudo apt update --quiet && sudo apt install --yes \
 
 # Increase the amount of inotify file watchers
 # https://github.com/guard/listen/wiki/Increasing-the-amount-of-inotify-watchers
+# https://unix.stackexchange.com/a/13757
+# https://askubuntu.com/a/155426
 echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
