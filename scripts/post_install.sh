0b3c932352d247d2bdf631f3b4b1c1ccbb80935c
David Beermann <mail@davidbeermann.com>
2019-11-15 18:06:32 +0100

    add FileZilla and Shotwell

--- scripts/post_install.sh
+++ scripts/post_install.sh
@@ -2,14 +2,18 @@
 
 # Usefull binaries from distribution
 # ----------------------------------
+# FileZilla – FTP Client: https://wiki.filezilla-project.org/Client_Installation#Installing_on_GNU.2FLinux_and_other_Unix.28-like.29_systems
 # GNOME Sushi – File Preview in Nautilus Desktop: https://gitlab.gnome.org/GNOME/sushi
 # Peek – Screencast utility: https://github.com/phw/peek
+# Shotwell –  Personal Photo Manager: https://wiki.gnome.org/Apps/Shotwell
 sudo apt update --quiet && sudo apt install --yes \
+  filezilla \
   gnome-sushi \
   gnome-tweaks \
   google-chrome-stable \
   htop \
   peek \
+  shotwell \
   slack-desktop \
   thunderbird \
   vim
